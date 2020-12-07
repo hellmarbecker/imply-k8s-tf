c933144dd3bc9a039ab86e389b24bd1961f85b66
Hellmar Becker <becker@hellmar-becker.de>
2020-12-07 13:46:26 +0100

    Create 00_setup_EKS.sh

---
+++ terraform/aws/00_setup_EKS.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+REGION=${1}
+CLUSTER=${2}
+
+# update kubeconfig with AWS cluster information
+# 
+aws eks --region ${REGION} update-kubeconfig --name ${CLUSTER}
+
+echo "Provisioning K8s cluster..."
+eksctl get cluster
+
+# _idempotent_ setup
+
+until kubectl cluster-info >/dev/null 2>&1; do
+    echo "kubeapi not available yet..."
+    sleep 3
+done
+
+echo " EKS cluster created"
+eksctl get cluster
